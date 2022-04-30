b170458344962fa7fba594eb20519f5b5a57e0ff
Everton Claudino <evertonclaudinoit@gmail.com>
2022-04-30 11:38:28 +0100

    Update Shop.js

--- src/Components/pages/Shop.js
+++ src/Components/pages/Shop.js
@@ -6,8 +6,7 @@ import tshirt3 from '../../img/t-shirt/t-shirt3.png';
 import tshirt4 from '../../img/t-shirt/t-shirt4.png';
 import tshirt5 from '../../img/t-shirt/t-shirt5.png';
 import tshirt6 from '../../img/t-shirt/t-shirt6.png';
-
-import Carousel from '../carousel/Carousel'
+import Seta from '../../img/Seta.png'
 
 function Shop() {
     var storage = []
@@ -29,8 +28,60 @@ function Shop() {
     }
 
     return (
-        <Carousel />
-       
+        <div className={stylesC.containerCarousel}>
+            <div className={stylesC.carousel}>
+                <div className={stylesC.item_Carousel}>
+                    <div className={stylesC.image}>
+                        <img src={tshirt1} alt='T-Shirt 1' />
+                    </div>
+                    <div className={stylesC.info}>
+                        <span className={stylesC.infoName}>Blusa Cinza com caimento</span>
+                        <span className={stylesC.infoPreco}>39,50&euro;</span>
+                        <button className={stylesC.info_btn} onClick={(event) => buy("Blusa Cinza", 39.50)}>Comprar</button>
+                    </div>
+                </div>
+                <div className={stylesC.item_Carousel}>
+                    <div className={stylesC.image}>
+                        <img src={tshirt2} alt='T-Shirt 2' />
+                    </div>
+                    <div className={stylesC.info}>
+                        <span className={stylesC.infoName}>Blusa Cinza Euphoria</span>
+                        <span className={stylesC.infoPreco}>35,90&euro;</span>
+                        <button className={stylesC.info_btn} onClick={(event) => buy("Blusa Cinza Euphoria", 35.90)}>Comprar</button>
+                    </div>
+                </div>
+                <div className={stylesC.item_Carousel}>
+                    <div className={stylesC.image}>
+                        <img src={tshirt3} alt='T-Shirt 3' />
+                    </div>
+                    <div className={stylesC.info}>
+                        <span className={stylesC.infoName}>Jaqueta Preta Element</span>
+                        <span className={stylesC.infoPreco}>50,80&euro;</span>
+                        <button className={stylesC.info_btn} onClick={(event) => buy("Jaqueta Preta Element", 50.80)}>Comprar</button>
+                    </div>
+                </div>
+                <div className={stylesC.item_Carousel}>
+                    <div className={stylesC.image}>
+                        <img src={tshirt4} alt='T-Shirt 4' />
+                    </div>
+                    <div className={stylesC.info}>
+                        <span className={stylesC.infoName}>Blusa Manga Longa Preta</span>
+                        <span className={stylesC.infoPreco}>28,99&euro;</span>
+                        <button className={stylesC.info_btn} onClick={(event) => buy("Blusa Manga Longa Preta", 28.99)}>Comprar</button>
+                    </div>
+                </div>
+                <div className={stylesC.item_Carousel}>
+                    <div className={stylesC.image}>
+                        <img src={tshirt5} alt='T-Shirt 5' />
+                    </div>
+                    <div className={stylesC.info}>
+                        <span className={stylesC.infoName}>Blusa Basica Branca</span>
+                        <span className={stylesC.infoPreco}>9,99&euro;</span>
+                        <button className={stylesC.info_btn} onClick={(event) => buy("Blusa Basica Branca", 9.99)}>Comprar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 
 }
